75d606edd3bb56276cee8a8759d5a6a1d343421b
kienphamtrung <kien.pham12345@hcmut.edu.vn>
2023-12-16 11:50:15 +0700

    Change color

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -36,7 +36,7 @@ class _MyHomeState extends State<HomePage> {
   var imageHeight = false;
   int _currentIndex = 0;
   int ivisible = 1;
-  int _accountColor = const Color.fromARGB(255, 132, 181, 53).value;
+  int _accountColor = const Color.fromARGB(255, 121, 180, 137).value;
   int _cameraColor = Colors.grey.value;
   int _settingsColor = Colors.grey.value;
   int _galleryColor = Colors.grey.value;
@@ -45,7 +45,7 @@ class _MyHomeState extends State<HomePage> {
     return Scaffold(
 appBar: AppBar(
   title: const Text("Home Page"),         
-backgroundColor: const Color.fromARGB(255, 132, 181, 53), // Use the RGB value for matcha
+backgroundColor: const Color.fromARGB(255, 121, 180, 137), // Use the RGB value for matcha
 
   
   actions: [
@@ -181,7 +181,7 @@ backgroundColor: const Color.fromARGB(255, 132, 181, 53), // Use the RGB value f
                       label: 'Gallery',
                     ),
                   ],
-                  selectedItemColor:  const Color.fromARGB(255, 132, 181, 53),
+                  selectedItemColor:  const Color.fromARGB(255, 121, 180, 137),
                   unselectedItemColor: Colors.grey,
                   currentIndex: _currentIndex,
                   onTap: (index) {
@@ -196,7 +196,7 @@ backgroundColor: const Color.fromARGB(255, 132, 181, 53), // Use the RGB value f
                       _cameraColor = Colors.grey.value;
                       _settingsColor = Colors.grey.value;
                       _galleryColor = Colors.grey.value;
-                       Color newColor = const Color.fromARGB(255, 132, 181, 53);
+                       Color newColor = const Color.fromARGB(255, 121, 180, 137);
 
 
                       // Set the selected icon color to blue
